b91cb59bba4c98b4752f7d6c4570078b7fced8ef
alimehndi <alimehndi99@gmail.com>
2025-05-20 23:07:16 +0530

    removed unneccesory api

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,6 @@ connectDb();
 const app = express();
 
 const port = 3001;
-app.get('/', (req : Request,res : Response) => {
-    res.send(`gtd`);
-})
 
 app.listen(port , () => {
     console.log(`Server is successfully running on port ${port}`);
